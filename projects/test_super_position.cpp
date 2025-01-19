a9b8a2474fae5e2209de4eec5859026d5b273642
jika_ubuntu_on_ThinkPad_L13_Gen2 <jika1366119@gmail.com>
2025-01-19 10:56:36 +0900

    new

--- projects/test_super_position.cpp
+++ projects/test_super_position.cpp
@@ -26,28 +26,56 @@ int main() {
     double tau_0 = 1.0;
     int sample_amount = pow(10,8);
     int t_first_l = 16;
-    int t_last_l = 10000000;
+    int t_last_l = pow(10,7);
 
 
 
-    vector<unsigned int> walk_1_1 = generate_power_law_point_process(1.1, tau_0, sample_amount);
-    std::tuple<double, double, std::vector<int>, std::vector<double>> result1_1 = dfa_F2(walk_1_1, 1.1, t_first_l, t_last_l);
+    const int num_trials = 100;
+    vector<vector<double>> results1_1(num_trials);
+    vector<vector<double>> results1_5(num_trials); 
+    vector<vector<double>> results1_5_and_1_1(num_trials);
+    vector<int> time_scales; // 時間スケールを保存
 
-    vector<unsigned int> walk_1_5 = generate_power_law_point_process(1.5, tau_0, sample_amount);
-    std::tuple<double, double, std::vector<int>, std::vector<double>> result1_5 = dfa_F2(walk_1_5, 1.5, t_first_l, t_last_l);
+    for (int trial = 0; trial < num_trials; trial++) {
+        vector<unsigned int> walk_1_1 = generate_power_law_point_process(1.1, tau_0, sample_amount);
+        auto result1_1 = dfa_F2(walk_1_1, 1.1, t_first_l, t_last_l);
+        results1_1[trial] = std::get<3>(result1_1);
 
-    vector<unsigned int> walk_1_5_and_1_1 = super_position(walk_1_5, walk_1_1);
-    std::tuple<double, double, std::vector<int>, std::vector<double>> result1_5_and_1_1 = dfa_F2(walk_1_5_and_1_1, 1.16, t_first_l, t_last_l);
+        vector<unsigned int> walk_1_5 = generate_power_law_point_process(1.5, tau_0, sample_amount);
+        auto result1_5 = dfa_F2(walk_1_5, 1.5, t_first_l, t_last_l);
+        results1_5[trial] = std::get<3>(result1_5);
 
-    for (size_t i = 0; i < std::get<3>(result1_1).size(); ++i) {
+        vector<unsigned int> walk_1_5_and_1_1 = super_position(walk_1_5, walk_1_1);
+        auto result1_5_and_1_1 = dfa_F2(walk_1_5_and_1_1, 1000.0, t_first_l, t_last_l);
+        results1_5_and_1_1[trial] = std::get<3>(result1_5_and_1_1);
 
-        cout << std::get<2>(result1_1)[i] << ",";
-        cout << std::get<3>(result1_1)[i] << ",";
-        cout << std::get<3>(result1_5)[i] << ",";
-        cout << "sum_of_left, ";
-        cout << std::get<3>(result1_1)[i] + std::get<3>(result1_5)[i];
-        cout << "             target";
-        cout << std::get<3>(result1_5_and_1_1)[i] << endl;
+        if (trial == 0) {
+            time_scales = std::get<2>(result1_1);
+        }
     }
+
+    // 各時間スケールごとに平均を計算
+    for (size_t i = 0; i < results1_1[0].size(); ++i) {
+        double avg1_1 = 0, avg1_5 = 0, avg1_5_and_1_1 = 0;
+        
+        for (int trial = 0; trial < num_trials; trial++) {
+            avg1_1 += results1_1[trial][i];
+            avg1_5 += results1_5[trial][i];
+            avg1_5_and_1_1 += results1_5_and_1_1[trial][i];
+        }
+        
+        avg1_1 /= num_trials;
+        avg1_5 /= num_trials;
+        avg1_5_and_1_1 /= num_trials;
+
+        cout << time_scales[i] << ",";
+        cout << avg1_1 << ",";
+        cout << avg1_5 << ",";
+        cout << "sum_of_left, ";
+        cout << avg1_1 + avg1_5;
+        cout << "             target";
+        cout << avg1_5_and_1_1 << endl;
+    }
+    
 }
 
